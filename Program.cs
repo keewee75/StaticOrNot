ba9e8d8e19888cd71bb1aaa7ec8c1bd42b1c7088
keewee75 <marko.kivi@gmail.com>
2022-07-07 09:39:45 +0200

    Mer om Id

--- Program.cs
+++ Program.cs
@@ -4,28 +4,28 @@
 
 using StaticOrNot;
 
-Random rnd = new Random();
-for (int i = 0; i < 5; i++)
-{
-MyStatics.LuckyNumber = rnd.Next(1, 101);
-Console.WriteLine(MyStatics.LuckyNumber);
+//Random rnd = new Random();
+//for (int i = 0; i < 5; i++)
+//{
+//MyStatics.LuckyNumber = rnd.Next(1, 101);
+//Console.WriteLine(MyStatics.LuckyNumber);
 
-}
+//}
 
 //Att använda en non-static metod kräver instansiering
 MyStatics instans = new MyStatics();
 instans.Name = "Object 1";
-int startValue = 0;
-MyStatics.LuckyNumber = startValue;
-for (int i = 0; i < 5; i++)
-{
-    instans.Incrementing();
-    Console.Write(MyStatics.LuckyNumber + ", ");
-}
+//int startValue = 0;
+//MyStatics.LuckyNumber = startValue;
+//for (int i = 0; i < 5; i++)
+//{
+//    instans.Incrementing();
+//    Console.Write(MyStatics.LuckyNumber + ", ");
+//}
 
 MyStatics instans2 = new MyStatics();
 instans2.Name = "Object 2";
-Console.WriteLine(MyStatics.LuckyNumber);
+//Console.WriteLine(MyStatics.LuckyNumber);
 
 //Skriv ut de 2 objektens innehåll
 Console.WriteLine($"Id: {instans.Id}, Name: {instans.Name} , LuckyNumber value: {MyStatics.LuckyNumber}");
